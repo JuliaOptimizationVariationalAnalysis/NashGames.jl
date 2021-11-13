3a13d212fff7975b0ff52cb8e62adbc9f6d6b478
Documenter.jl <documenter@juliadocs.github.io>
2021-11-13 19:56:15 +0000

    build based on 7cc035e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NashGames","category":"page"},{"location":"#NashGames","page":"Home","title":"NashGames","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NashGames.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NashGames]","category":"page"}]
+}
